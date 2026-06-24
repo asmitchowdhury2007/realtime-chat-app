efcc19032225b1b3aa25b09a8a1692f1c44942b4
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-06-24 23:42:26 +0530

    Setup chat section

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express');
 const app = express();
 const http = require("http");
 const { Server } = require('socket.io');
+const path = require('path');
 
 // server creation
 const server = http.createServer(app); 
 const io = new Server(server);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on("connection", (socket)=>{
+    console.log("A new user entered");
+    
+})
+
+
 server.listen(5000, ()=> console.log("Server started..."));
